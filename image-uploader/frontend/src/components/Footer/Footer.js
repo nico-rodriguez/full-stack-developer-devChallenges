423b16856648d2d81f8f9458f33007469309d3e5
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-04-22 16:31:39 -0300

    Fix spacing in footer

--- image-uploader/frontend/src/components/Footer/Footer.js
+++ image-uploader/frontend/src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css';
 export function Footer({}) {
   return (
     <footer>
-      created by{' '}
+      created by
       <a href='https://github.com/nico-rodriguez/full-stack-developer-devChallenges/tree/master/image-uploader'>
         nico-rodriguez
-      </a>{' '}
+      </a>
       - <a href='https://devchallenges.io/'>devChallenges.io</a>
     </footer>
   );
