11eda5c8089f2bfba4d49f4337976097864e617c
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-04-22 16:15:08 -0300

    Set footer link to repository

--- image-uploader/frontend/src/components/Footer/Footer.js
+++ image-uploader/frontend/src/components/Footer/Footer.js
@@ -4,7 +4,10 @@ import './Footer.css';
 export function Footer({}) {
   return (
     <footer>
-      created by <a href='https://github.com/nico-rodriguez'>nico-rodriguez</a>{' '}
+      created by{' '}
+      <a href='https://github.com/nico-rodriguez/full-stack-developer-devChallenges/tree/master/image-uploader'>
+        nico-rodriguez
+      </a>{' '}
       - <a href='https://devchallenges.io/'>devChallenges.io</a>
     </footer>
   );
