6e0f758f1e5a8888c877ad29d2557b7ffc918d6a
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-08-17 14:28:30 -0300

    fix: Send error messages as expected by the frontend

--- image-uploader/backend/middleware/errorHandler.js
+++ image-uploader/backend/middleware/errorHandler.js
@@ -4,19 +4,19 @@ function errorHandler(err, req, res, next) {
   if (err.name === 'MulterError') {
     res.status(400);
     if (err.code === 'LIMIT_FILE_COUNT') {
-      res.json({ error: 'Too many files' });
+      res.json('Too many files');
     } else if (err.code === 'LIMIT_FILE_SIZE') {
-      res.json({ error: 'File too large (max. 4MB)' });
+      res.json('File too large (max. 4MB)');
     } else {
       res.status(400);
-      res.json({ error: 'Unknown error' });
+      res.json('Unknown error');
     }
   } else if (err.name === 'Error' && err.message === 'File type not allowed') {
     res.status(400);
-    res.json({ error: err.message });
+    res.json(err.message);
   } else {
     res.status(500);
-    res.json({ error: 'Unknown error' });
+    res.json('Unknown error');
   }
 }
 
