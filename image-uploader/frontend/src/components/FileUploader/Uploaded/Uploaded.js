969f35e945c49f73036b73f2eab29cd62a276e8c
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-08-18 13:22:09 -0300

    fix: Set 'anonymous' to crossOrigin attribute

--- image-uploader/frontend/src/components/FileUploader/Uploaded/Uploaded.js
+++ image-uploader/frontend/src/components/FileUploader/Uploaded/Uploaded.js
@@ -11,7 +11,7 @@ const Uploaded = ({ imageURL }) => {
     <div id='file-uploaded'>
       <CheckIcon />
       <h1>Uploaded Successfully!</h1>
-      <img src={imageURL} alt='' crossOrigin />
+      <img src={imageURL} alt='' crossOrigin='anonymous' />
       <div className='image-url'>
         <div className='image-text-url'>{imageURL}</div>
         <button onClick={handleCopyLink}>Copy Link</button>
