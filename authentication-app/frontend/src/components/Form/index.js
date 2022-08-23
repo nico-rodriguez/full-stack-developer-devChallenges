6a68bbc09371cd0ddf3f56e8d8aada6898898b37
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-08-23 16:01:51 -0300

    refactor: Remove twitter and facebook login buttons

--- authentication-app/frontend/src/components/Form/index.js
+++ authentication-app/frontend/src/components/Form/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import facebookLogo from './Facebook.svg';
 import githubLogo from './Github.svg';
 import googleLogo from './Google.svg';
-import twitterLogo from './Twitter.svg';
 
 import './Form.css';
 
@@ -41,12 +39,6 @@ export function Form({ buttonText, handleFormData }) {
         <div className='alternatives__items'>
           <a href='#'>
             <img src={googleLogo} alt='Google logo' />
-          </a>
-          <a href='#'>
-            <img src={facebookLogo} alt='Facebook logo' />
-          </a>
-          <a href='#'>
-            <img src={twitterLogo} alt='Twitter logo' />
           </a>
           <a href='http://localhost:5000/api/v1/auth/github'>
             <img src={githubLogo} alt='Github logo' />
